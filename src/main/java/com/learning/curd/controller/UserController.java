961040d78c3fa33db24c6d715a99aed4b7051a27
kaushal kumar <kaushal.k.0014@gmail.com>
2025-06-15 21:23:52 +0530

    added spring spring security

---
+++ src/main/java/com/learning/curd/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.learning.curd.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.curd.entiry.Employee;
+import com.learning.curd.service.EmployeeService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private EmployeeService service;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Integer id) {
+		Employee employee = service.getEmployeeById(id);
+
+		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+	}
+
+	@GetMapping("/getByName")
+	public ResponseEntity<Employee> getEmployeeByName(@RequestParam(name="name") String name) {
+		Employee employee = service.getEmployeeByName(name);
+		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Employee>> getAllEmployee(){
+		List<Employee> list = service.getAllEmployee();
+		return new ResponseEntity<List<Employee>>(list, HttpStatus.OK);
+	}
+}
