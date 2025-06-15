f952fb7f9c7c77952f609141650d37fafd51d6f1
kaushal kumar <kaushal.k.0014@gmail.com>
2025-06-15 21:41:22 +0530

    jwt secpring security dto

---
+++ src/main/java/com/learning/curd/security/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.learning.curd.security.dto;
+
+public class AuthResponse {
+	
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
